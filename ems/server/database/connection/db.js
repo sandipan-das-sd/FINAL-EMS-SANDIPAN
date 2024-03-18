ceb26b27877ccd734a8072a7e1c4eb9cdef73346
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-18 20:55:23 +0530

    db connection modified

--- ems/server/database/connection/db.js
+++ ems/server/database/connection/db.js
@@ -5,13 +5,12 @@ dotenv.config();
 
 const Connection = async () => {
   // const { DB_USERNAME, DB_PASSWORD } = process.env;
-  mongo_URL=`mongodb+srv://pyqplatform:4lvW8IbQPvKRETfB@cluster0.3tzpejq.mongodb.net/curd_ems?retryWrites=true&w=majority&appName=Cluster0`
+ const  mongo_URL=`mongodb+srv://pyqplatform:4lvW8IbQPvKRETfB@cluster0.3tzpejq.mongodb.net/curd_ems?retryWrites=true&w=majority&appName=Cluster0`;
   
 
   try {
     await mongoose.connect(mongo_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
+     
     });
     console.log("Db Connection Successfully");
   } catch (error) {
